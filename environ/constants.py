ff68ecafd9a0a2e1a34813dcf56aa5e7a3620bb5
lyc0603 <luoyichencn110110@gmail.com>
2024-03-23 21:45:14 +0000

    config venv

---
+++ environ/constants.py
@@ -0,0 +1,8 @@
+"""This file contains the configuration settings for the market environment."""
+
+from environ.settings import PROJECT_ROOT
+
+# Paths
+DATA_PATH = PROJECT_ROOT / "data"
+FIGURE_PATH = PROJECT_ROOT / "figures"
+PROCESSED_DATA_PATH = PROJECT_ROOT / "processed_data"
